3c41100db6d16cf172238cf4dd74c7e18c35cf8f
Kalhara Perera <adorekasun@gmail.com>
2019-01-21 00:09:53 +0530

    API working pretty fine but dont try it for many Examples

--- SearchRepo/src/app/home/home.page.ts
+++ SearchRepo/src/app/home/home.page.ts
@@ -1,19 +1,32 @@
 import { Component } from '@angular/core';
 import { findLocaleData } from '@angular/common/src/i18n/locale_data_api';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
-  language: String;
-  FindData(key:any) {
-    console.log(key.target.value);
-    console.log(this.language);
+  language: string;
+
+  constructor(
+    public ServiceProvider: ServiceService
+  ) { }
+
+  public lang: string;
+  public key: string;
+  
+   FindData(key: any) {
+    this.key = (key.target.value);
+      this.lang = (this.language);
+
+    this.ServiceProvider.searchrepo(this.key, this.lang).subscribe(res => {
+
+      console.log(res);
+  });
 
 }
 
 }
-
-
